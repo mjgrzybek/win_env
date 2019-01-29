1cf4e7cf2d6456e21a92c0c1228038bd4a287124
Michal Grzybek <michal.grzybek@dynatrace.com>
2019-01-29 13:11:29 +0100

    7zip added

--- prep_env.ps1
+++ prep_env.ps1
@@ -1,7 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install -y notepadplusplus doublecmd
+choco install -y notepadplusplus doublecmd 7zip
 
 # use notepad++ to open any textfile
 cmd /c "ftype ""No Extension""=""notepad++.exe"" ""%1"""
