3765cca2dbc827c362f5862935d66fd627a4fda2
Michal Grzybek <michal.grzybek@dynatrace.com>
2019-01-29 12:54:02 +0100

    perp_env.ps1 install choco, notepad++, doublecmd; sets ftype for textfiles to notepad++

---
+++ perp_env.ps1
@@ -0,0 +1,14 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install -y notepadplusplus doublecmd
+
+# use notepad++ to open any textfile
+cmd /c "ftype ""No Extension""=""notepad++.exe"" ""%1"""
+foreach ($extension in @(".txt", ".log")) 
+{ 
+    $fileType = (cmd /c "assoc $extension") 
+    $fileType = $fileType.Split("=")[-1]  
+    cmd /c "ftype $fileType=""notepad++.exe"" ""%1""" 
+}
+cmd /c "ftype "".properties""=""notepad++.exe"" ""%1"""
